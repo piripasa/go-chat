a5eeb6829d81a9861ce279643e89bab580a95a24
Zaman <zaman.pasa@shohoz.net>
2019-02-07 13:35:40 +0600

    server started

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main()  {
+	// Start the server on localhost port 8000 and log any errors
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
