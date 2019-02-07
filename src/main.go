0cdff82bdcf00f6673db5f21d963204c9c9a242f
Zaman <zaman.pasa@shohoz.net>
2019-02-07 13:46:49 +0600

    handle websocket connections

--- src/main.go
+++ src/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
 
+var upgrader = websocket.Upgrader{}
+var clients = make(map[*websocket.Conn]bool)
+
 func main()  {
+	// Configure websocket route
+	http.HandleFunc("/ws", handleConnections)
+
 	// Start the server on localhost port 8000 and log any errors
 	log.Println("http server started on :8000")
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request)  {
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+
+	clients[ws] = true
 }
